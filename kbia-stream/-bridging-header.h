9ad43fc85e7775dbc8fe229010f2945992c3817e
Nathan Lawrence <lawrencen@missouri.edu>
2015-09-25 12:20:13 -0500

    Added StreamingKit Library.

---
+++ kbia-stream/-bridging-header.h
@@ -0,0 +1,14 @@
+//
+//  -bridging-header.h
+//  kbia-stream
+//
+//  Created by Nathan Lawrence on 9/25/15.
+//  Copyright © 2015 Nathan Lawrence. All rights reserved.
+//
+
+#ifndef _bridging_header_h
+#define _bridging_header_h
+
+#import "/StreamingKit/NSMutableArray+STKAudioPlayer.h"
+
+#endif /* _bridging_header_h */
